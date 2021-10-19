df6a67aa50964353bfcac44eaf5396b9c479d2c5
Craig Hillelson <craighillelson@gmail.com>
2021-10-19 09:53:22 -0400

    update commandwich.py
    achieved mvp

--- commandwich.py
+++ commandwich.py
@@ -31,45 +31,66 @@ def output_selected_element(label, element):
     print("{} {}".format(label, element))
 
 
-BREADS = [
-    'rye',
-    'sourdough',
-    'white',
-]
-BREAD = prompt_user_to_make_a_choice("\nWhat kind of bread would you like?",
-                                     BREADS)
-MEATS = [
-    'ham',
-    'roast beef',
-    'turkey',
-]
-MEAT = prompt_user_to_make_a_choice("\nWhat kind of meat would you like?",
-                                    MEATS)
-WITH_CHEESE = prompt_user_for_yes_or_no("\nWould you like cheese?")
-CHEESES = [
-    "american",
-    "cheddar",
-    "provelone",
-]
-if WITH_CHEESE == "yes":
-    CHEESE = prompt_user_to_make_a_choice("\nWhat kind of cheese would you "
-                                          "like?", CHEESES)
+def output_order_summary():
+    print("\norder summary")
+    for sandwich in SANDWICHES:
+        output_selected_element(f"\nnumber of sandwiches:", sandwich[6])
+        output_selected_element("bread:", sandwich[0])
+        output_selected_element("meat:", sandwich[1])
+        output_selected_element("cheese", sandwich[2])
+        output_selected_element("mayo:", sandwich[3])
+        output_selected_element("mustard:", sandwich[4])
+        output_selected_element("lettuce and tomato:", sandwich[5])
 
-WITH_MAYO = prompt_user_for_yes_or_no("\nWould you like mayo (yes or no)?")
-WITH_MUSTARD = prompt_user_for_yes_or_no("\nWould you like mustard "
-                                         "(yes or no)?")
-WITH_LETTUCE_AND_TOMATO = prompt_user_for_yes_or_no("\nWould you like lettuce "
-                                                    "and tomato (yes or no)?")
 
-NUMBER_OF_SANDWICHES = prompt_user_for_number_of_sandwiches("\nHow many "
-                                                            "sandwiches would "
-                                                            "you like?")
+SANDWICHES = []
 
-print("\nyour sandiwch order")
-output_selected_element("number of sandwiches:", NUMBER_OF_SANDWICHES)
-output_selected_element("- bread:", BREAD)
-output_selected_element("- meat:", MEAT)
-output_selected_cheese()
-output_selected_element("- mayo:", WITH_MAYO)
-output_selected_element("- mustard:", WITH_MUSTARD)
-output_selected_element("- lettuce and tomato:", WITH_LETTUCE_AND_TOMATO)
+while True:
+    BREADS = [
+        'rye',
+        'sourdough',
+        'white',
+    ]
+    BREAD = prompt_user_to_make_a_choice("\nWhat kind of bread would you like?",
+                                         BREADS)
+    MEATS = [
+        'ham',
+        'roast beef',
+        'turkey',
+    ]
+    MEAT = prompt_user_to_make_a_choice("\nWhat kind of meat would you like?",
+                                        MEATS)
+    WITH_CHEESE = prompt_user_for_yes_or_no("\nWould you like cheese "
+                                            "(yes or no?")
+    CHEESES = [
+        "american",
+        "cheddar",
+        "provelone",
+    ]
+    if WITH_CHEESE == "yes":
+        CHEESE = prompt_user_to_make_a_choice("\nWhat kind of cheese would you "
+                                              "like?", CHEESES)
+    else:
+        CHEESE = "no"
+
+    WITH_MAYO = prompt_user_for_yes_or_no("\nWould you like mayo (yes or no)?")
+    WITH_MUSTARD = prompt_user_for_yes_or_no("\nWould you like mustard "
+                                             "(yes or no)?")
+    WITH_LETTUCE_AND_TOMATO = prompt_user_for_yes_or_no("\nWould you like "
+                                                        "lettuce and tomato "
+                                                        "(yes or no)?")
+
+    NUMBER_OF_SANDWICHES = prompt_user_for_number_of_sandwiches("\nHow many "
+                                                                "sandwiches "
+                                                                "would you "
+                                                                "like?")
+
+    print("\nWould you like to order another sandwich?")
+    ORDER_ANOTHER = pyip.inputYesNo("> ", blank=True)
+    SANDWICH = (BREAD, MEAT, CHEESE, WITH_MAYO, WITH_MUSTARD,
+                WITH_LETTUCE_AND_TOMATO, NUMBER_OF_SANDWICHES)
+    SANDWICHES.append(SANDWICH)
+    if ORDER_ANOTHER == "no":
+        break
+
+output_order_summary()
