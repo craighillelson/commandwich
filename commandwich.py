64fd4d8b2619eb58d1cd8572e8f148dc2f6e7e59
Craig Hillelson <craighillelson@gmail.com>
2021-12-16 08:45:07 -0500

    update commandwich.py
    streamlined code, made it easier to read

--- commandwich.py
+++ commandwich.py
@@ -5,6 +5,46 @@ Command line sandwich ordering app.
 import pyinputplus as pyip
 
 
+def capture_user_choice(number):
+    return pyip.inputInt("> ", min=1, max=number)
+
+
+def output_selected_cheese():
+    if WITH_CHEESE == "yes":
+        print(f"- cheese: {CHEESE}")
+    else:
+        print("- no cheese")
+
+
+def output_selected_element(label, element):
+    print("{} {}".format(label, element))
+
+
+def output_order_summary():
+    print("\norder summary")
+    for sandwich in SANDWICHES:
+        output_selected_element("\nnumber of sandwiches:", sandwich[6])
+        output_selected_element("bread:", sandwich[0])
+        output_selected_element("meat:", sandwich[1])
+        output_selected_element("cheese", sandwich[2])
+        output_selected_element("mayo:", sandwich[3])
+        output_selected_element("mustard:", sandwich[4])
+        output_selected_element("lettuce and tomato:", sandwich[5])
+
+
+def output_options(dct):
+    for k, v in dct.items():
+        print(f"{k}. {v}")
+
+
+def present_options_and_capture_user_choice(dct, header):
+    print(header)
+    output_options(dct)
+    number = len(dct)
+    user_choice = capture_user_choice(number)
+    return dct[user_choice]
+
+
 def prompt_user_to_make_a_choice(header, lst):
     print(header)
     return pyip.inputMenu(lst, numbered=True)
@@ -20,34 +60,6 @@ def prompt_user_for_number_of_sandwiches(header):
     return pyip.inputInt("> ")
 
 
-def output_selected_cheese():
-    if WITH_CHEESE == "yes":
-        print(f"- cheese: {CHEESE}")
-    else:
-        print("- no cheese")
-
-
-def output_options(dct):
-    for k, v in dct.items():
-        print(f"{k}. {v}")
-
-
-def output_selected_element(label, element):
-    print("{} {}".format(label, element))
-
-
-def output_order_summary():
-    print("\norder summary")
-    for sandwich in SANDWICHES:
-        output_selected_element(f"\nnumber of sandwiches:", sandwich[6])
-        output_selected_element("bread:", sandwich[0])
-        output_selected_element("meat:", sandwich[1])
-        output_selected_element("cheese", sandwich[2])
-        output_selected_element("mayo:", sandwich[3])
-        output_selected_element("mustard:", sandwich[4])
-        output_selected_element("lettuce and tomato:", sandwich[5])
-
-
 SANDWICHES = []
 
 while True:
@@ -56,24 +68,21 @@ while True:
         2: 'sourdough',
         3: 'white',
     }
-    print("\nWhat kind of bread would you like?")
-    output_options(BREADS)
-    number_of_breads = len(BREADS)
-    user_choice = pyip.inputInt("> ", min=1, max=number_of_breads)
-    BREAD = BREADS[user_choice]
-    
+    BREAD = \
+    present_options_and_capture_user_choice(BREADS, "\nWhat kind of bread "
+                                            "would you like?")
+
     MEATS = {
         1: 'ham',
         2: 'roast beef',
         3: 'turkey',
     }
-    print("\nWhat kind of meat would you like?")
-    output_options(MEATS)
-    number_of_meats = len(MEATS)
-    user_choice = pyip.inputInt("> ", min=1, max=number_of_meats)
-    MEAT = MEATS[user_choice]
+    MEAT = \
+    present_options_and_capture_user_choice(MEATS, "\nWhat kind of meat would "
+                                            "you like?")
+
     WITH_CHEESE = prompt_user_for_yes_or_no("\nWould you like cheese "
-                                            "(yes or no?")
+                                            "(yes or no)?")
 
     if WITH_CHEESE == "yes":
         CHEESES = {
@@ -81,24 +90,23 @@ while True:
             2: "cheddar",
             3: "provelone",
         }
-        output_options(CHEESES)
-        number_of_cheeses = len(CHEESES)
-        user_choice = pyip.inputInt("> ", min=1, max=number_of_cheeses)
-        CHEESE = CHEESES[user_choice]
+        CHEESE = \
+        present_options_and_capture_user_choice(CHEESES, "\nWhat kind of "
+                                                "cheese would you like?")
     else:
         CHEESE = "no"
 
-    WITH_MAYO = prompt_user_for_yes_or_no("\nWould you like mayo (yes or no)?")
-    WITH_MUSTARD = prompt_user_for_yes_or_no("\nWould you like mustard "
-                                             "(yes or no)?")
-    WITH_LETTUCE_AND_TOMATO = prompt_user_for_yes_or_no("\nWould you like "
-                                                        "lettuce and tomato "
-                                                        "(yes or no)?")
+    WITH_MAYO = \
+    prompt_user_for_yes_or_no("\nWould you like mayo (yes or no)?")
+    WITH_MUSTARD = \
+    prompt_user_for_yes_or_no("\nWould you like mustard (yes or no)?")
+    WITH_LETTUCE_AND_TOMATO = \
+    prompt_user_for_yes_or_no("\nWould you like lettuce and tomato "
+                              "(yes or no)?")
 
-    NUMBER_OF_SANDWICHES = prompt_user_for_number_of_sandwiches("\nHow many "
-                                                                "sandwiches "
-                                                                "would you "
-                                                                "like?")
+    NUMBER_OF_SANDWICHES = \
+    prompt_user_for_number_of_sandwiches("\nHow many sandwiches would you "
+                                         "like?")
 
     print("\nWould you like to order another sandwich?")
     ORDER_ANOTHER = pyip.inputYesNo("> ", blank=True)
